6ad0fac6064f86260a5e0ee918f8a89289c24974
Deekshith <deekshithshettigar93@outlook.com>
2019-01-22 01:19:44 +0530

    my 1st commit

---
+++ src/main/java/com/qa/lips/baseclass.java
@@ -0,0 +1,10 @@
+package com.qa.lips;
+
+public class baseclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/qa/page/loginpage.java
@@ -0,0 +1,10 @@
+package com.qa.page;
+
+public class loginpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
