601f6b20ef307d3ffc1fe2ace57a05cc001654f4
Deekshith <deekshithshettigar93@outlook.com>
2019-01-22 11:52:31 +0530

    2nd commit added searchpage and modified login pagge

--- src/main/java/com/qa/page/loginpage.java
+++ src/main/java/com/qa/page/loginpage.java
@@ -2,9 +2,10 @@ package com.qa.page;
 
 public class loginpage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.err.println("login page");
+		System.out.println("login page 2");
 	}
 
 }
---
+++ src/main/java/com/qa/page/searchpage.java
@@ -0,0 +1,9 @@
+package com.qa.page;
+
+public class searchpage
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Search page");
+	}
+}
